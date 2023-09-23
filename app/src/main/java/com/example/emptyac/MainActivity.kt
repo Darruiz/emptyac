89a53589f918560d3a86fa89d7b42708cf5e1dde
Darruiz <99142969+Darruiz@users.noreply.github.com>
2023-09-22 22:40:31 -0300

    commit with nomear

---
+++ app/src/main/java/com/example/emptyac/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.emptyac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.emptyac.databinding.ActivityMainBinding
+
+
+class  MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            binding.btnname.setOnClickListener {
+
+                val nvm = binding.nameput.text.toString()
+
+                binding.tview.text = "Olá, " + nvm
+
+            }
+        }
+    }
