a286789fc1fd39e49f5b8f5de163f9f2c7985c13
Darruiz <99142969+Darruiz@users.noreply.github.com>
2023-10-01 19:36:14 -0300

    conversor de moedas com base em valor fixo

--- app/src/main/java/com/example/emptyac/MainActivity.kt
+++ app/src/main/java/com/example/emptyac/MainActivity.kt
@@ -4,23 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.emptyac.databinding.ActivityMainBinding
 
-
-class  MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            binding.btnname.setOnClickListener {
+        binding.convertButton.setOnClickListener {
+     
+            val euroAmount = binding.euroInput.text.toString().toDoubleOrNull()
 
-                val nvm = binding.nameput.text.toString()
+            if (euroAmount != null) {
+              
+                val conversionRate = 1.18 
 
-                binding.tview.text = "Olá, ${nvm}"
+              
+                val usdAmount = euroAmount * conversionRate
 
+               
+                binding.result.text = String.format("%.2f Euros = %.2f Dólares", euroAmount, usdAmount)
+            } else {
+                binding.result.text = "Valor em euros inválido"
             }
         }
     }
+}
